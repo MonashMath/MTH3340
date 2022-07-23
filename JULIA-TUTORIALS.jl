76592251ad2bfa224ac0fa8b7b6ce9da43a01e87
Santiago Badia <santiago.badia@monash.edu>
2022-07-23 18:50:05 +1000

    Create JULIA-TUTORIALS.jl

---
+++ JULIA-TUTORIALS.jl
@@ -0,0 +1,5 @@
+I have not created specific tutorials for basic Julia. The reason is that there are already excellent tutorials out there. 
+
+You can take a look at the tutorials [here](https://julialang.org/learning/tutorials/)
+
+I recommend to read this [here](https://www.matecdev.com/posts/julia-tutorial-science-engineering.html). It provides the basics of what we will need during this unit (and more). But you can certainly explore other tutorials in the list above. And please ask me if you have questions!
